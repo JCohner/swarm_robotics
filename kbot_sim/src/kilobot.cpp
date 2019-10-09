b6110e908a8f56d2283fabc417b58c3230b67682
JCohner <joshuacohen2019@u.northwestern.edu>
2019-10-09 17:53:25 -0500

    succesfully implemented algorithim that ensures of all homogenously coded robots only one appears green and one appears blue with all other robots appearing red

--- kbot_sim/src/kilobot.cpp
+++ kbot_sim/src/kilobot.cpp
@@ -12,27 +12,23 @@ class mykilobot : public kilobot
 
 	int msrx=0;
 	struct mydata {
-		unsigned int data1;
-		unsigned int data2;
+		//data structure where highest lowest in heard will live, initialize it with current id because it is both the highest and lowest id we have heard
+		unsigned int highest;
+		unsigned int lowest;
 	};
-
+	mydata keeper;
 	//main loop
 	void loop()
 	{
-		
-		
 		if(rxed==1)
 		{
-			if(out_message.data[0]%2==0)
-			{
-			set_color(RGB(1,0,0));
+			if (keeper.lowest == id){
+				set_color(RGB(0,0,1));
+			} 
+			if (keeper.highest == id){
+				set_color(RGB(0,1,0));
 			}
-			else
-			{
-			set_color(RGB(0,1,0));
-    			
-			}
-    			rxed=0;
+    		rxed=0;
 		}
 		
 
@@ -40,8 +36,8 @@ class mykilobot : public kilobot
 		//update message
 		out_message.type = NORMAL;
 		out_message.data[0] = id;
-		out_message.data[1] = 0;
-		out_message.data[2] = 0;
+		out_message.data[1] = keeper.highest;
+		out_message.data[2] = keeper.lowest;
 		out_message.crc = message_crc(&out_message);
 
 
@@ -50,12 +46,22 @@ class mykilobot : public kilobot
 	//executed once at start
 	void setup()
 	{
+		//already has a random id inhereted from the robot class
 		id=id&0xff;
+
+		keeper.highest = id;
+		keeper.lowest = id;
+		//sets type to 1
 		out_message.type = NORMAL;
+		//but 0 of 9 bits of data is bot ID
 		out_message.data[0] = id;
-		out_message.data[1] = 0;
-		out_message.data[2] = 0;
+		//could maybe have this as highest?
+		out_message.data[1] = keeper.highest;
+		//and this as lowest?
+		out_message.data[2] = keeper.lowest;
 		out_message.crc = message_crc(&out_message);
+		
+		//set to default color
 		set_color(RGB(0,0,1));
 		
 	}
@@ -83,9 +89,20 @@ class mykilobot : public kilobot
 	void message_rx(message_t *message, distance_measurement_t *distance_measurement)
 	{
 		distance = estimate_distance(distance_measurement);
-		out_message.data[0] = message->data[0];
-		out_message.data[1] = message->data[1];
-		out_message.data[2] = message->data[2];
+		//out_message.data[0] = message->data[0];
+		//out_message.data[1] = message->data[1];
+		//out_message.data[2] = message->data[2];
+		
+		if (message->data[1] > keeper.highest){
+			keeper.highest = message->data[1];
+			set_color(RGB(1,0,0));
+		}
+
+		if (message->data[2] < keeper.lowest){
+			keeper.lowest = message->data[2];
+			set_color(RGB(1,0,0));
+		}
+
 		rxed=1;
 	}
 };
