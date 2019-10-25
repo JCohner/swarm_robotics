dda228d9b32892299209d46f9e3cb0fd3fe7ed5d
JCohner <joshuacohen2019@u.northwestern.edu>
2019-10-24 21:19:18 -0500

    first gradient still working, data structure for future gradient implementation now in place

--- kbot_sim/src/kilobot.cpp
+++ kbot_sim/src/kilobot.cpp
@@ -13,9 +13,10 @@ class mykilobot : public kilobot
 	int msrx=0;
 	struct mydata {
 		//data structure where highest lowest in heard will live, initialize it with current id because it is both the highest and lowest id we have heard
-		unsigned short x;
-		unsigned short y;
-		unsigned char h_count;
+		unsigned short my_x;
+		unsigned short my_y;
+		unsigned short seed_pos[2][2];
+		unsigned char h_count[2];
 		unsigned short seed_list[2];
 		unsigned char seed_count;
 	};
@@ -25,18 +26,18 @@ class mykilobot : public kilobot
 	{
 		if(rxed==1)
 		{
-			// out_message.data[0] = (rid & 0xff00) >> 8; //id high
-			// out_message.data[1] = rid & 0x00ff; //id low
+			
+			out_message.data[0] = (rid & 0xff00) >> 8; //id high
+			out_message.data[1] = rid & 0x00ff; //id low
+			int i = 2;
+			for (int x = 0; x < my_info.seed_count; x++){
+				// printf("forwarding\n");
+				out_message.data[i + x] = my_info.seed_pos[x][0];
+				out_message.data[i + x + 1] = my_info.seed_pos[x][1];
+				out_message.data[i + x + 2] = my_info.h_count[x] + 1;
+			}
     		rxed=0;
 		}
-		
-		//update message
-		// out_message.type = NORMAL;
-		// out_message.data[0] = (rid & 0xff00) >> 8; //id high
-		// out_message.data[1] = rid & 0x00ff; //id low
-		// out_message.data[2] = keeper.lowest;
-		// out_message.crc = message_crc(&out_message);
-
 
 	}
 
@@ -50,27 +51,57 @@ class mykilobot : public kilobot
 		if (rid == 0xffff || rid == 0xfffe) {
 			// execute seed logic
 			printf("seed id is: %X\n", rid);
+			unsigned short x_short = (unsigned short)  pos[0];
+			unsigned short y_short = (unsigned short) pos[1];
 			set_color(RGB(0,0,1));
 
 			my_info.seed_list[my_info.seed_count++] = rid;
-			my_info.h_count = 0;
+			my_info.my_x = x_short;
+			my_info.my_y = y_short;
+			my_info.seed_pos[my_info.seed_count][0] = x_short;
+			my_info.seed_pos[my_info.seed_count][1] = y_short;
+			my_info.h_count[0] = 0;
+			my_info.h_count[1] = (unsigned char) 0xFE;
 			//sets type to 1
 			out_message.type = NORMAL;
 			out_message.data[0] = (rid & 0xff00) >> 8; //highbits of id
 			out_message.data[1] = rid & 0x00ff; //lowbits of id
 			
-			unsigned short x_short = (unsigned short)  pos[0];
-			unsigned short y_short = (unsigned short) pos[1];
+			
 
 			out_message.data[2] = (x_short & 0xff00) >> 8; //xpos hi
 			out_message.data[3] = (x_short & 0x00ff); //xpos low
 			out_message.data[4] = (y_short & 0xff00) >> 8; //ypos hi
 			out_message.data[5] = y_short & 0x00ff; //ypos low
-			out_message.data[6] = my_info.h_count + 1;
+			out_message.data[6] = my_info.h_count[0] + 1;
+			out_message.data[7] = 0xff; //xpos hi
+			out_message.data[8] = 0xff; //xpos low
+			out_message.data[9] = 0xff; //ypos hi
+			out_message.data[10] = 0xff; //ypos low
+			out_message.data[11] = my_info.h_count[1];
 			out_message.crc = message_crc(&out_message);
 		} else if (rid > 0) {
 			//execute member logic
+			memset(my_info.h_count, (unsigned char) 0xFE, 2);
 
+			out_message.type = NORMAL;
+			out_message.data[0] = (rid & 0xff00) >> 8; //highbits of id
+			out_message.data[1] = rid & 0x00ff; //lowbits of id
+			
+			// unsigned short x_short = (unsigned short)  pos[0];
+			// unsigned short y_short = (unsigned short) pos[1];
+
+			out_message.data[2] = 0xff; //xpos hi
+			out_message.data[3] = 0xff; //xpos low
+			out_message.data[4] = 0xff; //ypos hi
+			out_message.data[5] = 0xff; //ypos low
+			out_message.data[6] = my_info.h_count[0];
+			out_message.data[7] = 0xff; //xpos hi
+			out_message.data[8] = 0xff; //xpos low
+			out_message.data[9] = 0xff; //ypos hi
+			out_message.data[10] = 0xff; //ypos low
+			out_message.data[11] = my_info.h_count[1];
+			out_message.crc = message_crc(&out_message);
 			set_color(RGB(1,1,1));
 		}
 		
@@ -110,11 +141,35 @@ class mykilobot : public kilobot
 	//receives message
 	void message_rx(message_t *message, distance_measurement_t *distance_measurement)
 	{
-		//distance = estimate_distance(distance_measurement);
-		if (message->data[6] == 1){
-			set_color(RGB(1,0,0));
+		// distance = estimate_distance(distance_measurement);
+		// char hop = message->data[6];
+		// if (hop < my_info.h_count){
+		// 	my_info.h_count = hop;
+		// 	// printf("hop: %d\n", hop);
+		// 	// set_color(RGB(0,1,0));
+		// 	if (hop == 1){
+		// 		printf("slurp\n");
+		// 		set_color(RGB(1,0,0));
+		// 	}
+		// }
+		unsigned char hop = message->data[6];
+		unsigned short x = (message->data[0] << 8) | (message->data[1] & 0xff);
+		// if (x == 0xffff | x == 0xfffe){
+		if (hop < my_info.h_count[0]){
+			if (hop == 1){
+				set_color(RGB(1,0,0));
+				my_info.h_count[0] = (unsigned char) hop;
+			} else if (hop == 2){
+				set_color(RGB(1,0,1));
+				my_info.h_count[0] = (unsigned char) hop;
+			} else{
+				printf("heard something else %d", hop);
+			}
+			my_info.seed_count++;
 		}
-		
+		// } else{
+			// printf("boner msg %X\n", x);
+		// }
 		// if (message->data[1] > keeper.highest){
 		// 	keeper.highest = message->data[1];
 		// 	//set_color(RGB(1,0,0));
@@ -126,5 +181,6 @@ class mykilobot : public kilobot
 		// }
 
 		rxed=1;
+		
 	}
 };
