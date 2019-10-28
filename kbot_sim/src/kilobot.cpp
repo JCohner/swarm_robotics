4ffd058d5fbc96a3c111d195464f28284494c913
JCohner <joshuacohen2019@u.northwestern.edu>
2019-10-28 16:16:40 -0500

    not working commit

--- kbot_sim/src/kilobot.cpp
+++ kbot_sim/src/kilobot.cpp
@@ -36,7 +36,7 @@ class mykilobot : public kilobot
 				out_message.data[i + x + 2] = (my_info.seed_pos[x][1] & 0xff00) >> 8;
 				out_message.data[i + x + 3] = my_info.seed_pos[x][1] & 0xff;
 				out_message.data[i + x + 4] = my_info.h_count[x] + 1;
-				// printf("sending message with hopcount: %d\n", out_message.data[i + x + 4]);
+				printf("sending message with hopcount: %d\n", out_message.data[i + x + 4]);
 			}
     		rxed=0;
 		}
@@ -70,20 +70,26 @@ class mykilobot : public kilobot
 			out_message.data[0] = (rid & 0xff00) >> 8; //highbits of id
 			out_message.data[1] = rid & 0x00ff; //lowbits of id
 			
-			/*seed 1 position*/
-			out_message.data[2] = (x_short & 0xff00) >> 8; //xpos hi
-			out_message.data[3] = (x_short & 0x00ff); //xpos low
+			out_message.data[2] = (rid & 0xff00) >> 8; //seed id hi
+			out_message.data[3] = rid & 0x00ff; //seed id low
 
-			out_message.data[4] = (y_short & 0xff00) >> 8; //ypos hi
-			out_message.data[5] = y_short & 0x00ff; //ypos low
+			/*seed 1 position*/
+			out_message.data[4] = (x_short & 0xff00) >> 8; //xpos hi
+			out_message.data[5] = (x_short & 0x00ff); //xpos low
+
+			out_message.data[6] = (y_short & 0xff00) >> 8; //ypos hi
+			out_message.data[7] = y_short & 0x00ff; //ypos low
 
 			/*hopcount to that seed position*/
-			out_message.data[6] = my_info.h_count[0] + 1;
-			out_message.data[7] = 0xff; //xpos hi
-			out_message.data[8] = 0xff; //xpos low
-			out_message.data[9] = 0xff; //ypos hi
-			out_message.data[10] = 0xff; //ypos low
-			out_message.data[11] = my_info.h_count[1];
+			out_message.data[8] = my_info.h_count[0] + 1;
+
+			out_message.data[9] = 0x0; //seed2 id hi
+			out_message.data[10] = 0x0; //seed2 id low
+			out_message.data[11] = 0xff; //xpos hi
+			out_message.data[12] = 0xff; //xpos low
+			out_message.data[13] = 0xff; //ypos hi
+			out_message.data[14] = 0xff; //ypos low
+			out_message.data[15] = my_info.h_count[1];
 			out_message.crc = message_crc(&out_message);
 		} else if (rid > 0) {
 			//execute member logic
@@ -95,32 +101,23 @@ class mykilobot : public kilobot
 			
 			// unsigned short x_short = (unsigned short)  pos[0];
 			// unsigned short y_short = (unsigned short) pos[1];
-
-			out_message.data[2] = 0xff; //xpos hi
-			out_message.data[3] = 0xff; //xpos low
-			out_message.data[4] = 0xff; //ypos hi
-			out_message.data[5] = 0xff; //ypos low
-			out_message.data[6] = my_info.h_count[0];
-			out_message.data[7] = 0xff; //xpos hi
-			out_message.data[8] = 0xff; //xpos low
-			out_message.data[9] = 0xff; //ypos hi
-			out_message.data[10] = 0xff; //ypos low
-			out_message.data[11] = my_info.h_count[1];
+			out_message.data[2] = 0x0; //seed 1 id hi
+			out_message.data[3] = 0x0; //seed 1 id low
+			out_message.data[4] = 0xff; //xpos hi
+			out_message.data[5] = 0xff; //xpos low
+			out_message.data[6] = 0xff; //ypos hi
+			out_message.data[7] = 0xff; //ypos low
+			out_message.data[8] = my_info.h_count[0];
+			out_message.data[9] = 0x0; //seed 2 id hi
+			out_message.data[10] = 0x0; //seed 2 id low
+			out_message.data[11] = 0xff; //xpos hi
+			out_message.data[12] = 0xff; //xpos low
+			out_message.data[13] = 0xff; //ypos hi
+			out_message.data[14] = 0xff; //ypos low
+			out_message.data[15] = my_info.h_count[1];
 			out_message.crc = message_crc(&out_message);
 			set_color(RGB(1,1,1));
 		}
-		
-		
-		
-		// unsigned short test_x = (out_message.data[2] << 8) | (out_message.data[3] & 0xff);
-		// unsigned short test_y = (out_message.data[4] << 8) |(out_message.data[5] & 0xff);
-
-		//set to default color
-		// set_color(RGB(0,0,1));
-		// if (id < 0){
-		// 	printf("hey im negative!\n");
-		// 	set_color(RGB(1,0,0));
-		// }
 		
 	}
 
@@ -147,18 +144,31 @@ class mykilobot : public kilobot
 	void message_rx(message_t *message, distance_measurement_t *distance_measurement)
 	{
 		distance = estimate_distance(distance_measurement);
-		// char hop = message->data[6];
-		// if (hop < my_info.h_count){
-		// 	my_info.h_count = hop;
-		// 	// printf("hop: %d\n", hop);
-		// 	// set_color(RGB(0,1,0));
-		// 	if (hop == 1){
-		// 		printf("slurp\n");
-		// 		set_color(RGB(1,0,0));
-		// 	}
-		// }
-		unsigned char hop = message->data[6];
-		unsigned short sender = (message->data[0] << 8) | (message->data[1] & 0xff);
+
+		unsigned char hop = message->data[8];
+		unsigned char hop2 = message->data[15];
+		unsigned short seed1 = 0;
+		unsigned short x_pos1;
+		unsigned short y_pos1;
+		unsigned short seed2 = 0;
+		unsigned short x_pos2;
+		unsigned short y_pos2;
+		//listen to all hop fields that aren't uninitialized
+		if (hop != 0xfe){
+			seed1 = (message->data[2] << 8) | (message->data[3] & 0xff);
+			x_pos1 = (message->data[4] << 8) | (message->data[5] & 0xff);
+			y_pos1 = (message->data[6] << 8) | (message->data[7] & 0xff);
+		} 
+		if (hop2 != 0xfe){
+			seed2 = (message->data[9] << 8) | (message->data[10] & 0xff);
+			x_pos2 = (message->data[11] << 8) | (message->data[12] & 0xff);
+			y_pos2 = (message->data[13] << 8) | (message->data[14] & 0xff);
+		} 
+		
+		//compare apples to apples
+		//seeds align case
+
+
 		// if (x == 0xffff | x == 0xfffe){
 		if (hop < my_info.h_count[0] && hop != 0){
 			if (hop % 2 == 1){
@@ -167,11 +177,13 @@ class mykilobot : public kilobot
 				set_color(RGB(0,1,0));
 			} 
 			my_info.h_count[0] = (unsigned char) hop;
-			my_info.seed_pos[0][0] = message->data[4];
-			my_info.seed_pos[0][1] = message->data[5];
+			my_info.seed_pos[0][0] = x_pos1;
+			my_info.seed_pos[0][1] = y_pos1;
+			my_info.seed_list[0] = seed1; 			
 			printf("hop count set to: %d\n", my_info.h_count[0]);
 			my_info.seed_count++;
 		}
+
 
 
 		rxed=1;
