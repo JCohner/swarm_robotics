d553f830f1dd8971816e52320f70904574907f6d
JCohner <joshuacohen2019@u.northwestern.edu>
2019-11-16 13:23:35 -0600

    used proportional control to get single robot to turn towards light

--- kbot_sim/src/kilobot.cpp
+++ kbot_sim/src/kilobot.cpp
@@ -8,33 +8,39 @@ class mykilobot : public kilobot
 	message_t out_message;
 	int rxed=0;
 	float theta;
-
+	char motorR = 50;
+	char motorL = 45; 
 
 	int msrx=0;
 
 	//main loop
 	void loop()
 	{	
-		// printf("angle to light = %f , angle to last robot message = %f \n\r",angle_to_light,theta); 
-		if (id == 0){
-			set_color(RGB(1,1,1));
-		} else if (id == 1){
-			set_color(RGB(1,0,1));
-		} else if (id == 2){
+		printf("angle to light = %f , angle to last robot message = %f \n\r",angle_to_light,theta); 
+		// if (id == 0){
+		// 	set_color(RGB(1,1,1));
+		// } else if (id == 1){
+		// 	set_color(RGB(1,0,1));
+		// } else if (id == 2){
+		// 	set_color(RGB(1,0,0));
+		// }
+		spinup_motors();
+		if (angle_to_light < .1){
+			set_color(RGB(0,0,1));
+			motorR -= 5;
+		} else {
 			set_color(RGB(1,0,0));
 		}
+		set_motors(motorR, motorL);
+
 
 	}
 
 	//executed once at start
 	void setup()
 	{
-		
 		out_message.type = NORMAL;
-		out_message.crc = message_crc(&out_message);
-		printf("my id is: %d\n\n", id);
-
-		
+		out_message.crc = message_crc(&out_message);	
 	}
 
 	//executed on successfull message send
