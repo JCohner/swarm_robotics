7bddf62da054a21051353c66fa685e9c2b5fb23d
JCohner <joshuacohen2019@u.northwestern.edu>
2019-11-16 17:28:44 -0600

    got them clustering at light!

--- kbot_sim/src/kilobot.cpp
+++ kbot_sim/src/kilobot.cpp
@@ -15,7 +15,10 @@ class mykilobot : public kilobot
 	int prev_t = 0;
 	int t1 = 100;
 	int t2 = 50;
+	int t3 = 500;
 	double error;
+	int angle_flag = 0;
+
 
 	int state = 0;
 
@@ -25,26 +28,61 @@ class mykilobot : public kilobot
 	void loop()
 	{	
 		curr_t = kilo_ticks;
-		printf("prev t is: %d\t curr t is: %d\n", prev_t, curr_t);
-		if ((curr_t >= prev_t + t1) && (curr_t <= prev_t + t1 + t2)) {
-			motorR = 50;
-		}  else{
+		error = angle_to_light/ (2 * 3.14159265358979324);
+
+		// printf("prev t is: %d\t curr t is: %d\n", prev_t, curr_t);
+		if ((curr_t >= prev_t + t1) && (curr_t <= prev_t + t1 + t2) && (!angle_flag)) {
+			if (error < 0){
+				motorR = 50;
+				motorL = 0;
+			} else {
+				motorR = 0;
+				motorL = 50;
+			}
+		}
+		else if ((curr_t >= prev_t + t1) && (curr_t <= prev_t + t1 + t3) && (angle_flag)){
+			if (abs(error) > 0.05){
+				// t2 = 50;
+				set_color(RGB(1,1,1));
+				angle_flag = 0;
+			} else {
+				motorR = 50;
+				motorL = 50;
+			}	
+		}
+		else {
+			if (curr_t >= prev_t + t1 + t2){
+				prev_t = curr_t;
+				
+				t2 = (int)(50 * abs(error));
+				if (abs(error) < 0.01){
+					t2 = 0;
+					set_color(RGB(1,0,1));
+					angle_flag = 1;
+				}
+			}
 			motorR = 0;
 		}
 
-		if (curr_t >= prev_t + t1 + t2){
-			prev_t = curr_t;
-			error = angle_to_light/ (2 * 3.14159265358979324);
-			t2 = (int)(100 * abs(error));
-			if (abs(error) < 0.05){
-				t2 = 0;
-				motorR = 0;
-				set_color(RGB(1,0,1));
-			}
-		}
+		// if ((curr_t >= prev_t + t1) && (curr_t <= prev_t + t1 + t3) && (angle_flag)) {
+		// 	motorR = 50;
+		// }  else {
+		// 	if (curr_t >= prev_t + t1 + t2){
+		// 		prev_t = curr_t;
+		// 		error = angle_to_light/ (2 * 3.14159265358979324);
+		// 		t2 = (int)(50 * abs(error));
+		// 		if (abs(error) < 0.01){
+		// 			t2 = 0;
+		// 			set_color(RGB(1,0,1));
+		// 			angle_flag = 1;
+		// 		}
+		// 	}
+		// 	motorR = 0;
+		// } 
+
 		printf("motorR is %d\n", motorR);
 		spinup_motors();
-		set_motors(motorR, 0);
+		set_motors(motorR, motorL);
 		
 
 		// printf("angle to light = %f , angle to last robot message = %f \n\r",angle_to_light,theta); 
