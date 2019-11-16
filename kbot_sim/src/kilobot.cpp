c4249c46713ccd90708575eb4bb6f1ae5d645973
JCohner <joshuacohen2019@u.northwestern.edu>
2019-11-16 16:17:04 -0600

    get all robots to face center with open loop, closed loop combo, on to walking

--- kbot_sim/src/kilobot.cpp
+++ kbot_sim/src/kilobot.cpp
@@ -10,13 +10,44 @@ class mykilobot : public kilobot
 	float theta;
 	char motorR = 50;
 	char motorL = 45; 
+	
+	int curr_t;
+	int prev_t = 0;
+	int t1 = 100;
+	int t2 = 50;
+	double error;
+
+	int state = 0;
 
 	int msrx=0;
 
 	//main loop
 	void loop()
 	{	
-		printf("angle to light = %f , angle to last robot message = %f \n\r",angle_to_light,theta); 
+		curr_t = kilo_ticks;
+		printf("prev t is: %d\t curr t is: %d\n", prev_t, curr_t);
+		if ((curr_t >= prev_t + t1) && (curr_t <= prev_t + t1 + t2)) {
+			motorR = 50;
+		}  else{
+			motorR = 0;
+		}
+
+		if (curr_t >= prev_t + t1 + t2){
+			prev_t = curr_t;
+			error = angle_to_light/ (2 * 3.14159265358979324);
+			t2 = (int)(100 * abs(error));
+			if (abs(error) < 0.05){
+				t2 = 0;
+				motorR = 0;
+				set_color(RGB(1,0,1));
+			}
+		}
+		printf("motorR is %d\n", motorR);
+		spinup_motors();
+		set_motors(motorR, 0);
+		
+
+		// printf("angle to light = %f , angle to last robot message = %f \n\r",angle_to_light,theta); 
 		// if (id == 0){
 		// 	set_color(RGB(1,1,1));
 		// } else if (id == 1){
@@ -24,15 +55,11 @@ class mykilobot : public kilobot
 		// } else if (id == 2){
 		// 	set_color(RGB(1,0,0));
 		// }
-		spinup_motors();
-		if (angle_to_light < .1){
-			set_color(RGB(0,0,1));
-			motorR -= 5;
-		} else {
-			set_color(RGB(1,0,0));
-		}
-		set_motors(motorR, motorL);
-
+		
+		//turn for kiloticks + 5 
+		
+		
+		// printf("%d\n", kilo_ticks);
 
 	}
 
@@ -40,7 +67,10 @@ class mykilobot : public kilobot
 	void setup()
 	{
 		out_message.type = NORMAL;
-		out_message.crc = message_crc(&out_message);	
+		out_message.crc = message_crc(&out_message);
+		
+		set_color(RGB(1,1,1));	
+		spinup_motors();
 	}
 
 	//executed on successfull message send
